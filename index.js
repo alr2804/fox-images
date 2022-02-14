0e555a2b30c0200e497f85ace053ec32981ef1d3
Andree L <andree.lr28@gmail.com>
2022-02-13 20:24:20 -0500

    clear function

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import { registerImage } from "./lazy.js";
 
-
+//Function that generates random number between the limits of the API
 const random = () => {
     return Math.floor(Math.random()*(122-1)) + 1
 }
 
-
+//Use DOM manipulation to create the images
 const createImageNode = () => {
 
+    //Create the image container
     const container = document.createElement("div");
     container.id = "container"
 
-
+    //Create the image inside
     const image = document.createElement("img");
     image.width = "300";
 
-
+    //Save the srtc info
     image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`; 
 
     container.appendChild(image);
@@ -23,13 +24,13 @@ const createImageNode = () => {
     return container;
 }
 
-
-const newContainer = createImageNode();
+//select the nodes from HTML
 const mountNode = document.getElementById("images-container");
+const addButton = document.getElementById("addButton");
+const clearButton = document.getElementById("clearButton");
 
 
-
-const addButton = document.querySelector("button");
+//functions for the buttons
 const addImage = () => {
     const newImage = createImageNode();
 
@@ -37,4 +38,13 @@ const addImage = () => {
     registerImage(newImage);
 }
 
+//Remove all child nodes
+const clearImages = () => {
+    while(mountNode.firstChild){
+        mountNode.removeChild(mountNode.firstChild);
+    }
+}
+
+
 addButton.addEventListener("click", addImage);
+clearButton.addEventListener("click", clearImages);
